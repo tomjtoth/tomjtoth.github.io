6e8d2869811026a7d202afce17bdb612023ddb2b
Tamás Tóth <tamas.toth@helsinki.fi>
2023-09-03 20:53:25 +0300

    disabled visitor_countdown(), to be continued later

--- visitors.js
+++ visitors.js
@@ -1,26 +1,30 @@
-function visitor_countdown() {
-    setTimeout(_ => {
-        const
-            MIN = 60,
-            HOURS = 60 * MIN,
-            DAYS = 24 * HOURS,
-            diff = Math.floor((new Date(visitors[0][1]) - new Date()) / 1000),
-            d = Math.floor(diff / DAYS),
-            h = Math.floor((diff % DAYS) / HOURS),
-            m = Math.floor((diff % HOURS) / MIN),
-            s = diff % 60;
+class Visitor {
 
-        if (view() == 'about') {
-            if (diff > 0) {
-                spn_vstr.textContent = ` ${d} nap ${h}:${m}:${s} múlva`;
-                visitor_countdown();
-            } else {
-                spn_vstr.parentNode.classList.add('hu');
-                spn_vstr.parentNode.innerHTML = `${visitors[0][0]} IN DA HOUSE!!!!`;
+    constructor() {
+        setTimeout(_ => {
+            const
+                MIN = 60,
+                HOURS = 60 * MIN,
+                DAYS = 24 * HOURS,
+                diff = Math.floor((new Date(Visitor.visitors[0][1]) - new Date()) / 1000),
+                d = Math.floor(diff / DAYS),
+                h = Math.floor((diff % DAYS) / HOURS),
+                m = Math.floor((diff % HOURS) / MIN),
+                s = diff % 60;
+
+            if (view() == 'about') {
+                if (diff > 0) {
+                    this.spn_vstr.textContent = ` ${d} nap ${h}:${m}:${s} múlva`;
+                    new Visitor();
+                } else {
+                    this.spn_vstr.parentNode.classList.add('hu');
+                    this.spn_vstr.parentNode.innerHTML = `${this.visitors[0][0]} IN DA HOUSE!!!!`;
+                }
             }
-        }
-    }, 1000);
-}
+        }, 1000);
+    }
 
-const spn_vstr = document.querySelector('span#visitor-text');
-const visitors = [['Bálint + dr. NŐ', '2023-09-01T06:30:00Z', 'hu']]
+    static spn_vstr = document.querySelector('span#visitor-text');
+    static visitors = [['Bálint + dr. NŐ', '2023-09-01T06:30:00Z', 'hu']]
+
+}
