410f2c11dff3ca918d5600b2b993d0d3954140de
Tamás Tóth <tomjtoth@gmail.com>
2024-03-24 20:31:57 +0200

    `arch.sh` mostly complete w/o user config

--- arch.sh
+++ arch.sh
@@ -15,40 +15,21 @@ function add_to_fstab() {
 function log() {
     if [ "$1" == "err" ]; then
         shift
-        printf '\033[1;31mERROR\033[0m: %s' "$*"
+        printf '\n\t\033[1;31mERROR\033[0m: \033[1m%s\033[0m\n\n' "$*"
     else
-        printf '\033[1m==> \033[0m\033[90m%s\033[0m\n' "$*"
+        printf '\033[1m==> \033[93m%s\033[0m\n' "$*"
     fi
 }
 
 
-log setting makepkg flags
-sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j'"$(nproc)"'"/' /etc/makepkg.conf
-# [ -n "$(which rustup)" ] && rustup default stable
+
+log configuring systemd journals
+mkdir /etc/systemd/journald.conf.d 2>/dev/null
+printf '%s\n' [Journal] SystemMaxUse=50M \
+    > /etc/systemd/journald.conf.d/00-journal-size.conf
 
 
-log configuring SSH
-printf '%-20s %s\n' \
-    Port "${SSH_PORT:-55522}" \
-    AllowGroups wheel \
-    PermitRootLogin no \
-    > /etc/ssh/sshd_config.d/01_wheel.conf
-
-
-log enabling swap on zram
-printf "zram" > /etc/modules-load.d/zram.conf
-printf '%s, %s, %s, %s, %s, %s\n' \
-    'ACTION=="add"' \
-    'KERNEL=="zram0"' \
-    'ATTR{comp_algorithm}="zstd"' \
-    'ATTR{disksize}="1G"' \
-    'RUN="/usr/bin/mkswap -U clear /dev/%k"' \
-    'TAG+="systemd"' \
-    > /etc/udev/rules.d/99-zram.rules
-add_to_fstab /dev/zram0 none swap defaults,pri=100,discard 0 0
-
-
-if [ ! -f /etc/locale.conf ]; then
+if ! grep -q LC_TIME /etc/locale.conf; then
     log setting localization
 
     LOCALES=(en_US fi_FI hu_HU sv_FI)
@@ -57,16 +38,19 @@ if [ ! -f /etc/locale.conf ]; then
     done
 
     while true; do
-        read -erp "pick default locale: " LCL_IDX
+        read -erp "pick default locale: " lcl_idx
         # it's a number and is less than the LOCALES.length
-        [[ "$LCL_IDX" =~ ^[[:digit:]]+$ && $LCL_IDX -lt ${#LOCALES[@]} ]] && break
+        [[ \
+            "$lcl_idx" =~ ^[0-9]+$ && \
+            $lcl_idx -lt ${#LOCALES[@]} \
+        ]] && break
         log err invalid index
     done
 
     printf '%s=en_US.UTF-8\n' LANG LANGUAGE LC_MESSAGES \
         > /etc/locale.conf
 
-    printf 'LC_%s='"${LOCALES[$LCL_IDX]}"'.UTF-8\n' \
+    printf 'LC_%s='"${LOCALES[$lcl_idx]}"'.UTF-8\n' \
         ADDRESS \
         COLLATE \
         CTYPE \
@@ -82,37 +66,61 @@ if [ ! -f /etc/locale.conf ]; then
 fi
 
 
-log configuring systemd journals
-mkdir /etc/systemd/journald.conf.d
-printf '%s\n' [Journal] SystemMaxUse=50M \
-    > /etc/systemd/journald.conf.d/00-journal-size.conf
+if ! grep -q /dev/zram0 /etc/fstab; then
+    log enabling swap on zram
+    
+    printf "zram" > /etc/modules-load.d/zram.conf
+    printf '%s, %s, %s, %s, %s, %s\n' \
+        'ACTION=="add"' \
+        'KERNEL=="zram0"' \
+        'ATTR{comp_algorithm}="zstd"' \
+        'ATTR{disksize}="1G"' \
+        'RUN="/usr/bin/mkswap -U clear /dev/%k"' \
+        'TAG+="systemd"' \
+        > /etc/udev/rules.d/99-zram.rules
+    add_to_fstab /dev/zram0 none swap defaults,pri=100,discard 0 0
+fi
 
 
-log configuring sudoers
-SUDO_CONF=/etc/sudoers.d/01_wheel
-printf '%s\n' \
-    "# users in group wheel" \
-    "%wheel ALL=(ALL:ALL) ALL" \
-    > $SUDO_CONF
-chmod 0440 $SUDO_CONF
+log setting makepkg flags
+sed -i \
+    's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j'"$(nproc)"'"/' \
+    /etc/makepkg.conf
+# [ -n "$(which rustup)" ] && rustup default stable
 
 
-log adding primary user
-mapfile -t EXISTING_USERS < <(cut -d':' -f 1 < /etc/passwd)
-while true; do
-    read -er USERNAME
-    [[ " ${EXISTING_USERS[*]} " != *" $USERNAME "* ]] && break
-    log err "${USERNAME} is already taken, try another one: "
-done
-useradd -m -G wheel "$USERNAME"
+SUDO_CONF=/etc/sudoers.d/99_wheel
+if [ ! -f $SUDO_CONF ]; then
+    log configuring sudoers
+
+    printf '%s\n' \
+        "# users in group wheel" \
+        "%wheel ALL=(ALL:ALL) ALL" \
+        > $SUDO_CONF
+    chmod 0440 $SUDO_CONF
+fi
+
+if ! grep -q :1000: /etc/passwd; then
+    log "adding primary user"
+
+    mapfile -t EXISTING_USERS < <(cut -d':' -f 1 < /etc/passwd)
+    while true; do
+        read -erp "type primary username: " USERNAME
+        [[ " ${EXISTING_USERS[*]} " != *" $USERNAME "* ]] && break
+        log err "${USERNAME} is already taken, try another one!"
+    done
+    useradd -m -G wheel "$USERNAME"
+    passwd "$USERNAME"
+fi
 
 
-log installing paru
-curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/paru.tar.gz
-tar -xvzf paru.tar.gz
-cd paru || exit 1
-chown -R "$USERNAME:$USERNAME" .
-sudo -u  "$USERNAME"  makepkg -sic --noconfirm
+log installing packages
+sed -i \
+    -e 's/^#Color$/Color/m' \
+    -e 's/^NoProgressBar$/#NoProgressBar/m' \
+    -e 's/^#CheckSpace$/CheckSpace/m' \
+    -re 's/^(ParallelDownloads *=) *[0-9]+$/\1 20/m' \
+    /etc/pacman.conf
 
 
 pkgs=(
@@ -140,8 +148,31 @@ pkgs=(
     code docker docker-compose
 
 )
-log installing packages
-paru --noconfirm -Syyu "${pkgs[@]}"
+pacman --noconfirm -Syyu "${pkgs[@]}"
+
+
+if [ -z "$(which paru 2>/dev/null)" ]; then
+    log installing paru
+
+    curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/paru.tar.gz
+    tar -xvzf paru.tar.gz
+    cd paru || exit 1
+    chown -R "${USERNAME:=$()}:$USERNAME" .
+    # disable passwd auth temporarily
+    sed -i 's/ALL$/NOPASSWD: ALL/m' $SUDO_CONF
+    sudo -u  "$USERNAME"  makepkg -si --noconfirm
+    cd ..
+    rm -rf paru*
+    sed -i 's/NOPASSWD: ALL$/ALL/m' $SUDO_CONF
+fi
+
+
+log configuring SSH
+printf '%-20s %s\n' \
+    Port "${SSH_PORT:-55522}" \
+    AllowGroups wheel \
+    PermitRootLogin no \
+    > /etc/ssh/sshd_config.d/01_wheel.conf
 
 
 log enabling autologin in GNOME
@@ -191,7 +222,7 @@ conf_ntfs(){
 }
 TODO_NTFS
 
-
+# TODO: .bash_aliases
 # sudo -u "$USERNAME" dconf load - < curl -L ttj.hu/dconf-dump
 
 
