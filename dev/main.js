98c45aea1309952b077ad0ddbb0027cf390bb2c5
Tamás Tóth <tomjtoth@gmail.com>
2024-04-18 18:34:27 +0300

    implemented testView for mobile device testing

--- dev/main.js
+++ dev/main.js
@@ -2,9 +2,11 @@ import hashRouter from "./src/components/router.js"
 import aboutView from "./src/views/about.js";
 import thingsView from "./src/views/things.js";
 import loginView from "./src/views/login.js";
+import { testView } from './src/performance.js'
 
 hashRouter('body>nav', 'div#view', {
   about: aboutView,
   things: thingsView,
   login: loginView,
+  test: testView,
 });
--- dev/src/performance.js
+++ dev/src/performance.js
@@ -4,42 +4,73 @@ const x = n('div');
 document.body.appendChild(x);
 
 
-const timer = (func) => {
+const timer = (func, { toConsole = true }) => {
+  x.innerHTML = '';
   const start = new Date();
   func();
-  console.log('took:', (new Date() - start), 'ms to finish');
-}
-
-const innerHTML = (reps = 1000) => {
+  const result = `took: ${new Date() - start} ms to finish`;
   x.innerHTML = '';
-  timer(() => {
-    x.innerHTML = `
-    <ul style="color: red;">
-      <li class="jotain">
-        this is some thing
-        <a href="https://ttj.hu">and here's a link</a>
-      </li>
-    </ul>
-    `.repeat(reps);
-  })
 
+  if (toConsole) {
+    console.log(result);
+  } else {
+    alert(result)
+  }
 }
 
-const node = (reps = 1000) => {
-  timer(() => {
-    const arr = [];
-    while (--reps > 0) {
-      arr.push(
-        n(['ul', { style: { color: 'red' } }],
-          n(['li', { className: 'jotain' }],
-            'this is some thing',
-            n(['a', { href: 'https://ttj.hu' }], "and here's a link")
-          )
+const innerHTML = ({ reps }) => () => {
+  x.innerHTML = `
+  <ul style="color: red;">
+    <li class="jotain">
+      this is some thing
+      <a href="https://ttj.hu">and here's a link</a>
+    </li>
+  </ul>
+  `.repeat(reps);
+}
+
+const node = ({ reps }) => () => {
+  const arr = [];
+  while (--reps > 0) {
+    arr.push(
+      n({ _: 'ul', style: { color: 'red' } },
+        n({ _: 'li', className: 'jotain' },
+          'this is some thing',
+          n({ _: 'a', href: 'https://ttj.hu' }, "and here's a link")
         )
       )
-    }
-    x.replaceChildren(...arr);
-  })
+    )
+  }
+  x.replaceChildren(...arr);
 }
 
-export default { innerHTML, node }
+const testBoth = () => {
+
+  while (true) {
+    const reps = prompt('how many reps?');
+    if (!isNaN(reps)) {
+      [
+        innerHTML({ reps: Number(reps) }),
+        node({ reps: Number(reps) })
+      ].forEach(func => timer(func, { toConsole: false }))
+
+    } else {
+      alert('invalid number')
+    }
+
+    if (confirm('wanna quit?')) break;
+  }
+}
+
+const testView = () => {
+
+  const btn = n({
+    _: 'button',
+  }, 'testBoth');
+
+  btn.addEventListener('click', testBoth);
+
+  return btn
+}
+
+export { timer, innerHTML, node, testBoth, testView }
