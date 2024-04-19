06a976677b12f0f045a8127aa39fa93c31bf8e6e
Tamás Tóth <tomjtoth@gmail.com>
2024-04-19 13:51:51 +0300

    reworked login/register view for innerHTML

--- dev/src/views/login.js
+++ dev/src/views/login.js
@@ -1,38 +1,69 @@
-import { node as n } from "../utils.js";
+export const view = ({ view }) => {
 
-const view = () => {
-  const form = n('form',
+  const login = view === 'login';
 
-    n({ _: 'label', htmlFor: 'username' }, 'username:'),
-
-    n({
-      _: 'input',
-      name: 'username',
-      id: 'username',
-    }),
-
-    n('br'),
-
-    n({ _: 'label', htmlFor: 'password' }, 'password:'),
-
-    n({
-      _: 'input',
-      name: 'password',
-      id: 'password',
-      type: 'password'
-    }),
-
-    n('br'),
-
-    n('button', 'login')
-  );
+  const form = String.raw`
+        ${login ? '' : String.raw`
+          <label for="name">name:</label>
+          <input id="name" name="name" required />
+          <br />  
+        `}
+        <label for="username">username:</label>
+        <input id="username" name="username" required 
+          minlength="3" maxlength="8" />
+        <br />
+        <label for="password">password:</label>
+        <input id="password" name="password" type="password" required
+          minlength="8" />
+        <br />
+        ${login ? '' : String.raw`
+          <label for="verify">verify pw:</label>
+          <input id="verify" name="verify" type="password" required 
+            minlength="8" />
+          <br />  
+        `}
+        <button>${login ? 'login' : 'register'}</button>
+  `.toTag('form')
 
   form.addEventListener('submit', (e) => {
+
     e.preventDefault();
+
+    if (!login) {
+
+      const { target: {
+        password,
+        verify,
+        username,
+        name,
+      } } = e;
+
+      name.setCustomValidity(
+        (! /[A-Z](?:-?[a-z])* [A-Z](?:-?[a-z])*/.test(name.value))
+          ? 'format it as "Firstname Surname"'
+          : ''
+      );
+
+      username.setCustomValidity(
+        (! /^\w+$/.test(username.value))
+          ? 'must use only numbers, letters and underscores'
+          : ''
+      );
+
+      verify.setCustomValidity(
+        (password.value !== verify.value)
+          ? 'passwords don\'t match'
+          : ''
+      );
+
+    }
+
+    if (!e.target.reportValidity()) return;
+
     alert(`TODO: login as "${e.target.username.value
       }:${e.target.password.value
       }"`);
-  })
+  });
 
   return form;
 }
