56a90b00aa7c16941dfef09e2a5b72c8439cdb83
Tamás Tóth <tomjtoth@gmail.com>
2024-08-18 11:59:10 +0300

    hinting at disabling reminders.sh

--- linux/reminders.sh
+++ linux/reminders.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# remind users what aliases do
-LAST_REMINDER=$(cat ~/.last_user_reminder 2>/dev/null)
+
+FILE=~/.reminders_last_run
+LAST_REMINDER=$(cat $FILE 2>/dev/null)
 NOW=$(date +%s)
+
 if [ "${LAST_REMINDER:-0}" -lt $((NOW - 7*24*60*60)) ]; then
-	echo "$NOW" > ~/.last_user_reminder
+	echo "$NOW" > $FILE
 	printf '\t%s\n' \
+		'' \
+		'this is a weekly reminder' \
+		'run "echo 2000000000 > '$FILE'" to disable it' \
 		'' \
 		'typing "ööö" updates the whole system' \
 		'typing "öä xyz" searches for an application by the name "xyz"' \
