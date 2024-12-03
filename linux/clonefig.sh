e399c001ffc183da0d4cc1e25bf57f9ceb13fc5e
Tamás Tóth <tomjtoth@gmail.com>
2024-12-03 11:13:14 +0200

    function based logging/skipping w/ some overhead

--- linux/clonefig.sh
+++ linux/clonefig.sh
@@ -17,12 +17,23 @@ function add_to_fstab() {
 
 
 function log() {
-    printf '\033[1m==> \033[93m%s\033[0m\n' "$*"
+    local args="$*"
+    [ -z "$args" ] && args="${FUNCNAME[1]//_/ }"
+    printf '\033[1m==> \033[93m%s\033[0m\n' "$args"
 }
 
-function err() {
-    printf '\n\t\033[1;31mERROR\033[0m: \033[1m%s\033[0m\n\n' "$*"
+
+function skip() {
+    local args="$*"
+    [ -z "$args" ] && args="${FUNCNAME[1]//_/ }"
+    printf '\033[0;37m==> \033[90m%s\033[0m\n' "skipped $args"
 }
+
+
+function err() {
+    printf '\n\t\033[1;31mFAILED '"${FUNCNAME[1]//_/ }"'\033[0m: \033[1m%s\033[0m\n\n' "$*"
+}
+
 
 function run_as_1000() {
     sed -i 's/ALL$/NOPASSWD: ALL/m' $SUDO_CONF
@@ -40,256 +51,373 @@ function join_by_char() {
 
 # shellcheck disable=SC2046
 if [ $(id -u) -ne 0 ]; then
-	err this script is to be run as root
+	err this script must to be run as root
 	exit 1
 fi
 
 
-if [ ! -f $JOURNAL_CONF ]; then
-    log configuring systemd journals
+function limiting_systemd_journals_size() {
+    if [ ! -f $JOURNAL_CONF ]; then
+        log
 
-    mkdir ${JOURNAL_CONF%/*} 2>/dev/null
-    printf '%s\n' [Journal] SystemMaxUse=50M \
-        > $JOURNAL_CONF
-fi
+        mkdir ${JOURNAL_CONF%/*} 2>/dev/null
+        printf '%s\n' [Journal] SystemMaxUse=50M \
+            > $JOURNAL_CONF
 
-
-if ! grep -q LC_TIME /etc/locale.conf; then
-    log setting localization
-
-    LOCALES=(en_US fi_FI hu_HU sv_FI)
-    for lcl in "${!LOCALES[@]}"; do
-        printf '%i - %s\n' "$lcl" "${LOCALES[$lcl]}"
-    done
-
-    while true; do
-        read -erp "pick default locale: " lcl_idx
-        # it's a number and is less than the LOCALES.length
-        [[ \
-            "$lcl_idx" =~ ^[0-9]+$ && \
-            $lcl_idx -lt ${#LOCALES[@]} \
-        ]] && break
-        log err invalid index
-    done
-
-    printf '%s=en_US.UTF-8\n' LANG LANGUAGE LC_MESSAGES \
-        > /etc/locale.conf
-
-    printf 'LC_%s='"${LOCALES[$lcl_idx]}"'.UTF-8\n' \
-        ADDRESS \
-        COLLATE \
-        CTYPE \
-        IDENTIFICATION \
-        MEASUREMENT \
-        MONETARY \
-        NAME \
-        NUMERIC \
-        PAPER \
-        TELEPHONE \
-        TIME \
-        >> /etc/locale.conf
-fi
+        log DONE
+    else
+        skip
+    fi
+}
 
 
-if ! grep -q /dev/zram0 $FSTAB; then
-    log enabling swap on zram
+function configuring_locales() {
+    if ! grep -q LC_TIME /etc/locale.conf; then
+        log
 
-    RAM_100=$(free --mega | awk '/Mem:/ {print $2}')
-    RAM_020=$(( RAM_100 * 2 / 10))M
+        local lcl lcl_idx LOCALES=(en_US fi_FI hu_HU sv_FI)
+        for lcl in "${!LOCALES[@]}"; do
+            printf '%i - %s\n' "$lcl" "${LOCALES[$lcl]}"
+        done
 
-    printf "zram" > /etc/modules-load.d/zram.conf
-    printf '%s, %s, %s, %s, %s, %s\n' \
-        'ACTION=="add"' \
-        'KERNEL=="zram0"' \
-        'ATTR{comp_algorithm}="zstd"' \
-        'ATTR{disksize}="'$RAM_020'"' \
-        'RUN="/usr/bin/mkswap -U clear /dev/%k"' \
-        'TAG+="systemd"' \
-        > /etc/udev/rules.d/99-zram.rules
-    add_to_fstab /dev/zram0 none swap defaults,pri=100,discard 0 0
-fi
+        while true; do
+            read -erp "pick default locale: " lcl_idx
+            # it's a number and is less than the LOCALES.length
+            [[
+                "$lcl_idx" =~ ^[0-9]+$ &&
+                $lcl_idx -lt ${#LOCALES[@]}
+            ]] && break
+            err invalid index
+        done
 
+        printf '%s=en_US.UTF-8\n' LANG LANGUAGE LC_MESSAGES \
+            > /etc/locale.conf
 
-if [ ! -f $SUDO_CONF ]; then
-    log configuring sudoers
-
-    printf '%s\n' \
-        "# users in group wheel" \
-        "%wheel ALL=(ALL:ALL) ALL" \
-        > $SUDO_CONF
-    chmod 0440 $SUDO_CONF
-fi
-
-
-if ! grep -q :1000: /etc/passwd; then
-    log adding primary user
-
-    mapfile -t EXISTING_USERS < <(cut -d':' -f 1 < /etc/passwd)
-    while true; do
-        read -erp "type primary username: " USERNAME
-        [[ " ${EXISTING_USERS[*]} " != *" $USERNAME "* ]] && break
-        log err "${USERNAME} is already taken, try another one!"
-    done
-    useradd -m -G wheel,docker,boinc "$USERNAME"
-    passwd "$USERNAME"
-fi
-
-
-pkgs=(
-
-    # cli utils
-    nano mc htop ncdu networkmanager ntp pacman-contrib bluez-utils man-db ntfs-3g os-prober
-
-    # Desktop Environment
-    gdm gnome-shell gnome-keyring eog nautilus file-roller
-    gnome-terminal
-    gnome-control-center
-    gnome-shell-extension-appindicator
-    gnome-shell-extension-caffeine
-    xdg-desktop-portal-gnome
-    gnome-browser-connector
-    gnome-calculator gnome-tweaks
-    ttf-dejavu
-
-    # gui utils
-    evince vlc geany geany-plugins keepassxc
-
-    # video editing
-    obs-studio avidemux-qt
-
-    # Web
-    firefox chromium qbittorrent
-
-    # coding
-    code docker docker-buildx docker-compose
-
-    # openCL
-    intel-compute-runtime clinfo clpeak
-
-    boinc-nox boinctui
-
-)
-mapfile -t missing_pkgs < <(diff -B --changed-group-format='%<'\
-    --unchanged-group-format='' \
-    <(printf "%s\n" "${pkgs[@]}" | sort) \
-    <(printf "%s\n" "$(pacman -Qenq)" | sort)
-)
-if [ ${#missing_pkgs[@]} -ne 0 ]; then
-    log installing packages
-
-    sed -i \
-        -E 's/^#(Color)$/\1/m' \
-        -E 's/^(NoProgressBar)$/#\1/m' \
-        -E 's/^#(CheckSpace)$/\1/m' \
-        -re 's/^(ParallelDownloads) *= *[0-9]+$/\1 = 20/m' \
-        /etc/pacman.conf
-    pacman -Syyu "${missing_pkgs[@]}"
-fi
+        printf 'LC_%s='"${LOCALES[$lcl_idx]}"'.UTF-8\n' \
+            ADDRESS \
+            COLLATE \
+            CTYPE \
+            IDENTIFICATION \
+            MEASUREMENT \
+            MONETARY \
+            NAME \
+            NUMERIC \
+            PAPER \
+            TELEPHONE \
+            TIME \
+            >> /etc/locale.conf
+        
+        log DONE
+    else
+        skip
+    fi
+}
 
 
-if [ -z "$(which paru 2>/dev/null)" ]; then
-    log installing paru
+function enabling_zram() {
+    if ! grep -q /dev/zram0 $FSTAB; then
+        log
 
-    sed -i \
-        's/^#MAKEFLAGS="-j2"$/MAKEFLAGS="-j'"$(nproc)"'"/m' \
-        /etc/makepkg.conf
+        local ram100=$(free --mega | awk '/Mem:/ {print $2}')
+        local ram20=$(( ram100 * 2 / 10))M
 
-    mkdir /tmp/clonefig
-    cd /tmp/clonefig
-    curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/paru.tar.gz
-    tar -xvzf paru.tar.gz
-    chown -R 1000 .
-    cd paru
-    run_as_1000 makepkg -si --noconfirm
-    cd ..
-    rm -rf paru{,.tar.gz}
-fi
+        printf "zram" > /etc/modules-load.d/zram.conf
+        printf '%s, %s, %s, %s, %s, %s\n' \
+            'ACTION=="add"' \
+            'KERNEL=="zram0"' \
+            'ATTR{comp_algorithm}="zstd"' \
+            'ATTR{disksize}="'$ram20'"' \
+            'RUN="/usr/bin/mkswap -U clear /dev/%k"' \
+            'TAG+="systemd"' \
+            > /etc/udev/rules.d/99-zram.rules
 
-
-if [ ! -f $SSH_WHEEL_CONF ]; then
-    log configuring SSH
-
-    printf '%-20s %s\n' \
-        Port "${SSH_PORT:-55522}" \
-        AllowGroups wheel \
-        PermitRootLogin no \
-        > $SSH_WHEEL_CONF
-fi
+        add_to_fstab /dev/zram0 none swap defaults,pri=100,discard 0 0
+        
+        log DONE
+    else
+        skip
+    fi
+}
 
 
-if ! grep -q '^AutomaticLoginEnable=True$' /etc/gdm/custom.conf; then
-    log enabling autologin in GNOME
+function enabling_sudo_for_group_wheel() {
+    if [ ! -f $SUDO_CONF ]; then
+        log
 
-    sed -i '/^\[daemon\]$/aAutomaticLogin='"$USERNAME"'\nAutomaticLoginEnable=True' \
-        /etc/gdm/custom.conf
-fi
+        printf '%s\n' \
+            "# users in group wheel" \
+            "%wheel ALL=(ALL:ALL) ALL" \
+            > $SUDO_CONF
+        chmod 0440 $SUDO_CONF
 
-
-if [ ! -d /home/docker ]; then
-    log relocating docker to /home
-
-    mkdir /home/docker
-    ln -s /home/docker /var/lib/docker
-fi
-
-
-if [ "$(systemctl is-enabled gdm)" != "enabled" ]; then
-    log enabling services
-
-    for svc in docker gdm ntpd bluetooth NetworkManager; do
-        systemctl enable $svc
-    done
-fi
+        log DONE
+    else
+        skip
+    fi
+}
 
 
-# shellcheck disable=SC2016
-echo '
+function adding_primary_user() {
+    if ! grep -q :1000: /etc/passwd; then
+        log
 
-# getting the latest aliases online
-source <(curl -sSL https://tomjtoth.github.io/linux/bash_aliases) 2>/dev/null
+        local existing_users
 
-# and weekly reminders
-source <(curl -sSL https://tomjtoth.github.io/linux/reminders.sh) 2>/dev/null
+        mapfile -t existing_users < <(cut -d':' -f 1 < /etc/passwd)
+        while true; do
+            read -erp "type primary username: " user
+            [[ " ${existing_users[*]} " != *" $user "* ]] && break
+            err "${user} is already taken, try another one!"
+        done
+        useradd -m -G wheel,docker,boinc "$user"
+        passwd "$user"
+        
+        log DONE
+    else
+        skip
+    fi
+}
 
-' >> ~/.bashrc
+
+function installing_missing_packages() {
+    local missing_pkgs pkgs=(
+
+        # cli utils
+        nano mc htop ncdu networkmanager ntp pacman-contrib bluez-utils man-db ntfs-3g os-prober
+
+        # Desktop Environment
+        gdm gnome-shell gnome-keyring eog nautilus file-roller
+        gnome-terminal
+        gnome-control-center
+        gnome-shell-extension-appindicator
+        gnome-shell-extension-caffeine
+        xdg-desktop-portal-gnome
+        gnome-browser-connector
+        gnome-calculator gnome-tweaks
+        ttf-dejavu
+
+        # gui utils
+        evince vlc geany geany-plugins keepassxc
+
+        # video editing
+        obs-studio avidemux-qt
+
+        # Web
+        firefox chromium qbittorrent
+
+        # coding
+        code docker docker-buildx docker-compose
+
+        # openCL
+        intel-compute-runtime clinfo clpeak
+
+        boinc-nox boinctui
+
+    )
+    mapfile -t missing_pkgs < <(diff -B --changed-group-format='%<'\
+        --unchanged-group-format='' \
+        <(printf "%s\n" "${pkgs[@]}" | sort) \
+        <(printf "%s\n" "$(pacman -Qenq)" | sort)
+    )
+    if [ ${#missing_pkgs[@]} -ne 0 ]; then
+        log
+
+        sed -i \
+            -E 's/^#(Color)$/\1/m' \
+            -E 's/^(NoProgressBar)$/#\1/m' \
+            -E 's/^#(CheckSpace)$/\1/m' \
+            -re 's/^(ParallelDownloads) *= *[0-9]+$/\1 = 20/m' \
+            /etc/pacman.conf
+        pacman -Syyu "${missing_pkgs[@]}"
+
+        log DONE
+    else
+        skip
+    fi
+}
+
+
+function installing_paru() {
+    if [ -z "$(which paru 2>/dev/null)" ]; then
+        log
+
+        sed -i \
+            's/^#MAKEFLAGS="-j2"$/MAKEFLAGS="-j'"$(nproc)"'"/m' \
+            /etc/makepkg.conf
+
+        mkdir /tmp/clonefig
+        cd /tmp/clonefig
+        curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/paru.tar.gz
+        tar -xvzf paru.tar.gz
+        chown -R 1000 .
+        cd paru
+        run_as_1000 makepkg -si --noconfirm
+        cd ..
+        rm -rf paru{,.tar.gz}
+
+        log DONE
+    else
+        skip
+    fi
+}
+
+
+function configuring_ssh() {
+    if [ ! -f $SSH_WHEEL_CONF ]; then
+        log
+
+        printf '%-20s %s\n' \
+            Port 55522 \
+            AllowGroups wheel \
+            PermitRootLogin no \
+            > $SSH_WHEEL_CONF
+        
+        log DONE
+    else
+        skip
+    fi
+}
+
+
+function enabling_autologin_in_GDM() {
+    if ! grep -q '^AutomaticLoginEnable=True$' /etc/gdm/custom.conf; then
+        log
+
+        sed -i '/^\[daemon\]$/aAutomaticLogin='"$user"'\nAutomaticLoginEnable=True' \
+            /etc/gdm/custom.conf
+
+        log DONE
+    else
+        skip
+    fi
+}
+
+
+function relocating_docker(){
+    if [ ! -d /home/docker ]; then
+        log
+
+        mkdir /home/docker
+        ln -s /home/docker /var/lib/docker
+
+        log DONE
+    else
+        skip
+    fi
+}
+
+
+function enabling_systemd_services() {
+    if [ "$(systemctl is-enabled gdm)" != "enabled" ]; then
+        log
+
+        for svc in docker gdm ntpd bluetooth NetworkManager; do
+            systemctl enable $svc
+        done
+    else
+        skip
+    fi
+}
+
+
+function configuring_bashrc() {
+    if ! grep -q 'https:\/\/tomjtoth\.github\.io\/linux\/(bash_aliases|reminders\.sh)' ~/.bashrc; then
+        log
+
+        # shellcheck disable=SC2016
+        echo '
+
+        # getting the latest aliases online
+        source <(curl -sSL https://tomjtoth.github.io/linux/bash_aliases) 2>/dev/null
+
+        # and weekly reminders
+        source <(curl -sSL https://tomjtoth.github.io/linux/reminders.sh) 2>/dev/null
+
+        ' >> ~/.bashrc
+
+        log DONE
+    else
+        skip
+    fi
+}
+
 
 # this must be revised as selective keybindings should be passed on only
-# sudo -u \#1000 curl -L ttj.hu/linux/dconf-dump | dconf load /
+function importing_dconf_settings() {
+    log
 
-if [ -f $LVM_CONF ]; then
-    sed -i -E "s/^(\s*)#(\s*issue_discards)\s*=\s*0$/\1 \2 = 1/" $LVM_CONF
-fi
+    sudo -u \#1000 curl -L ttj.hu/linux/dconf-dump | dconf load /
 
-log adding option discard to $FSTAB
-
-UUIDs=($(lsblk -o uuid --filter 'ROTA != 1'))
-UUIDs=$(join_by_char "|" ${UUIDs[@]:1})
-
-sed -i -E "s/^(UUID=($UUIDs)\s+.+)(\s+[0-9]+\s+[0-9]+\s*)$/\1,discard \3/mg" $FSTAB
+    log DONE
+}
 
 
-if ! $(grep -qP 'Shutdown|Restart' $GRUB_CUSTOM); then
-    log adding GRUB menu entries
+function enabling_discards_in_LVM() {
+    if [ -f $LVM_CONF ]; then
+        log
 
-    printf '%s\n' \
-        'menuentry "Restart" { reboot }' \
-        'menuentry "Shutdown" { halt }' \
-        >> $GRUB_CUSTOM
-    grub-mkconfig -o /boot/grub/grub.cfg
-fi
+        sed -i -E "s/^(\s*)#(\s*issue_discards)\s*=\s*0$/\1 \2 = 1/" $LVM_CONF
 
-log DONE
+        log DONE
+    else
+        skip
+    fi
+}
 
 
-# shellcheck disable=SC2188
-<<SKIP_SWAPFILE
-# swapfile
-dd if=/dev/zero of=/swapfile bs=1M count=1k status=progress
-chmod 0600 /swapfile
-mkswap -U clear /swapfile
-echo "/swapfile none swap defaults 0 0" >> $FSTAB
-echo "vm.swappiness=10" > /etc/sysctl.d/99-swappiness.conf
-SKIP_SWAPFILE
+function adding_discard_options_in_fstab() {
+    log
+
+    local uuids=($(lsblk -o uuid --filter 'ROTA != 1'))
+    uuids=$(join_by_char "|" ${uuids[@]:1})
+
+    sed -i -E "s/^(UUID=($uuids)\s+.+)(\s+[0-9]+\s+[0-9]+\s*)$/\1,discard \3/mg" $FSTAB
+
+    log DONE
+}
+
+
+function adding_menu_entries_to_GRUB() {
+    if ! $(grep -qP 'Shutdown|Restart' $GRUB_CUSTOM); then
+        log
+
+        printf '%s\n' \
+            'menuentry "Restart" { reboot }' \
+            'menuentry "Shutdown" { halt }' \
+            >> $GRUB_CUSTOM
+        grub-mkconfig -o /boot/grub/grub.cfg
+
+        log DONE
+    else
+        skip
+    fi
+}
+
+
+# calling each func in order
+limiting_systemd_journals_size
+configuring_locales
+enabling_zram
+enabling_sudo_for_group_wheel
+adding_primary_user
+installing_missing_packages
+installing_paru
+configuring_ssh
+enabling_autologin_in_GDM
+relocating_docker
+enabling_systemd_services
+configuring_bashrc
+enabling_discards_in_LVM
+adding_discard_options_in_fstab
+adding_menu_entries_to_GRUB
+
+
+# # swapfile
+# dd if=/dev/zero of=/swapfile bs=1M count=1k status=progress
+# chmod 0600 /swapfile
+# mkswap -U clear /swapfile
+# echo "/swapfile none swap defaults 0 0" >> $FSTAB
+# echo "vm.swappiness=10" > /etc/sysctl.d/99-swappiness.conf
 
 
 # shellcheck disable=SC2188
