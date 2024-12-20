6d478ca1dcc29e02d194fa146a8d517670f810d5
Tamás Tóth <tomjtoth@gmail.com>
2024-12-20 22:36:19 +0200

    temp disclaimer finalized

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
+import { Routes, Route, useMatch, useNavigate } from "react-router";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div lang="en">
       <h1>Vite + React</h1>
       <p>
-        <a href="linux/clonefig.sh">clonefig</a> and{" "}
-        <a href="linux/bash_aliases">aliases</a>
-        should be available should be available
+        Migration in progress while Santa Clause is coming... The following
+        files
+        <ul>
+          <li>
+            <a href="recipies/README.md">recipies</a>
+          </li>
+          <li>
+            <a href="linux/clonefig.sh">clonefig</a>
+          </li>
+
+          <li>
+            <a href="linux/reminders.sh">reminders</a>
+          </li>
+
+          <li>
+            <a href="linux/bash_aliases">aliases</a>
+          </li>
+        </ul>
+        should be available. Everything else is unavailable.
       </p>
-    </>
+    </div>
   );
 }
 
