b403583af9bf9a64ec4a62c326687265ee6d1bf1
Tamás Tóth <tomjtoth@gmail.com>
2024-12-20 22:24:41 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <p>
-        <a href="linux/clonefig.sh">clonefig</a> should be available
+        <a href="linux/clonefig.sh">clonefig</a> and{" "}
+        <a href="linux/bash_aliases">aliases</a>
+        should be available should be available
       </p>
     </>
   );
