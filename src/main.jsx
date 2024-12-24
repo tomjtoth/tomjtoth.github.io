6548f6199f6d44344580548cc3655e9164f104f9
Tamás Tóth <tomjtoth@gmail.com>
2024-12-24 11:29:52 +0200

    disabled service-worker

--- src/main.jsx
+++ src/main.jsx
@@ -7,21 +7,21 @@ import "./main.css";
 import store from "./store.js";
 import App from "./App.jsx";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope: ",
-          registration.scope
-        );
-      })
-      .catch((error) => {
-        console.log("Service Worker registration failed: ", error);
-      });
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/service-worker.js")
+//       .then((registration) => {
+//         console.log(
+//           "Service Worker registered with scope: ",
+//           registration.scope
+//         );
+//       })
+//       .catch((error) => {
+//         console.log("Service Worker registration failed: ", error);
+//       });
+//   });
+// }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
