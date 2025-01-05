8c6691a0fc2ac4f96a8987025e59d72f3b85753f
Tamás Tóth <tomjtoth@gmail.com>
2025-01-05 07:34:12 +0200

    runes animation simplified using `:active` pseudo-class

--- src/components/Runes/index.jsx
+++ src/components/Runes/index.jsx
@@ -22,18 +22,8 @@ export default function () {
       </Header>
       <MainView
         className="runes"
-        onClick={({ target }) => {
-          if (target.tagName !== "IMG") return;
-
-          // Pause the animation
-          target.style.animationPlayState = "paused";
-
-          // Set the currentTime property to 0
-          target.style.animation = "none";
-          target.offsetHeight; /* trigger reflow */
-          target.style.animation = null;
-
-          setQueue(queue.concat(target.title));
+        onClick={(e) => {
+          if (e.target.tagName == "IMG") setQueue(queue.concat(e.target.title));
         }}
       >
         <div id="runes-drawing">{/* TODO: implement drawing by finger */}</div>
