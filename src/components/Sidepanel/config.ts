aea23d4a53213b0bd4cc6bd550010788c255543a
Tamás Tóth <tomjtoth@gmail.com>
2025-01-14 12:39:46 +0200

    refactored Sidepanel

---
+++ src/components/Sidepanel/config.ts
@@ -0,0 +1,12 @@
+export const links = [
+  { children: "alkuun", to: "/" },
+  { children: "látogatók", to: "/visitors", lang: "hu" },
+  { children: "Luxor sorsolás", to: "/luxor", lang: "hu" },
+  { children: "akunvalvonta", to: "/battery-monitor" },
+  { children: "ostoslista", to: "/shopping-list" },
+  { children: "låttext", to: "/lyrics", lang: "sv" },
+  {
+    children: "Arx Fatails minipeli",
+    to: "/arx-fatalis",
+  },
+];
--- src/components/Sidepanel/index.tsx
+++ src/components/Sidepanel/index.tsx
@@ -1,55 +1,16 @@
-import { useAppDispatch, useAppSelector } from "../../hooks";
 import { Link, useLocation } from "react-router";
+import { links } from "./config";
 
 import "./sidepanel.css";
 
-import { setSidepanel } from "../../reducers/sidepanel";
-
 import QRCode from "../QRCode";
+import Nav from "./Nav";
 
 export default function Sidepanel() {
-  const dispatch = useAppDispatch();
-  const className = `border1-e ${
-    useAppSelector((s) => s.sidepanel.active) ? " active" : ""
-  }`;
-
-  const links = [
-    { children: "alkuun", to: "/" },
-    { children: "látogatók", to: "/visitors", lang: "hu" },
-    { children: "Luxor sorsolás", to: "/luxor", lang: "hu" },
-    { children: "akunvalvonta", to: "/battery-monitor" },
-    { children: "ostoslista", to: "/shopping-list" },
-    { children: "låttext", to: "/lyrics", lang: "sv" },
-    {
-      children: "Arx Fatails minipeli",
-      to: "/arx-fatalis",
-    },
-  ];
-
   const { pathname } = useLocation();
 
   return (
-    <nav
-      {...{
-        id: "sidepanel",
-        className,
-
-        onMouseLeave: (e) => {
-          if ((e.target as HTMLElement).tagName === "NAV") {
-            dispatch(setSidepanel(false));
-          }
-        },
-
-        onClick: ({ target }) => {
-          const t = target as HTMLElement;
-          if (t.classList.contains("toggler")) {
-            dispatch(setSidepanel(false));
-          } else if (t.closest("#qr-code")) {
-            navigator.clipboard.writeText(window.location.toString());
-          }
-        },
-      }}
-    >
+    <Nav>
       <ul
         style={{
           listStyle: "none",
@@ -69,6 +30,6 @@ export default function Sidepanel() {
         ))}
       </ul>
       <QRCode value={`${window.location.pathname}/#${pathname}`} />
-    </nav>
+    </Nav>
   );
 }
