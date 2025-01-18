9f2d8517cf45283b3dac63fba089783e54d749a2
Tamás Tóth <tomjtoth@gmail.com>
2025-01-18 20:27:02 +0200

    added Header to main views

--- src/components/battery_monitor/mod.rs
+++ src/components/battery_monitor/mod.rs
@@ -1,8 +1,10 @@
+use super::Header;
 use dioxus::prelude::*;
 
 #[component]
 pub fn BatteryMonitor() -> Element {
     rsx! {
+        Header { title: &"akunvalvonta" }
         "WiP"
     }
 }
--- src/components/luxor/mod.rs
+++ src/components/luxor/mod.rs
@@ -1,8 +1,10 @@
+use crate::components::Header;
 use dioxus::prelude::*;
 
 #[component]
 pub fn Luxor() -> Element {
     rsx! {
+        Header { title: &"Luxor" }
         "WiP"
     }
 }
