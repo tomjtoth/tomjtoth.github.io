fe491235e3e189362198b32b071539c1b6d9abb7
Tamás Tóth <tomjtoth@gmail.com>
2025-01-28 11:07:38 +0200

    Revert "comparing dioxus vs yew wasm sizes in GH toolchain"
    This reverts commit bca9ebbde91f2c6ef56a2518bf0c5a54236267d2.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use dioxus::prelude::*;
-use gloo_net::http::Request;
 
-// mod components;
-// mod routes;
-// mod utils;
+mod components;
+mod routes;
+mod utils;
 
-// use components as c;
+use components as c;
 
 fn main() {
     dioxus::launch(App);
@@ -13,36 +12,18 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    // // init static resources
-    // c::audio::init();
+    // init static resources
+    c::audio::init();
 
-    // // read from localStorage
-    // c::sidepanel::init();
-    // c::luxor::init();
+    // read from localStorage
+    c::sidepanel::init();
+    c::luxor::init();
 
-    // // also fetching from server
-    // c::lyrics::init();
-    // c::shopping_list::init();
-
-    use_future(|| async {
-        let res = Request::get("/modal.mp3").send().await.unwrap();
-        if let Some(sad) = res.body() {
-            let x = web_sys::HtmlAudioElement::new().unwrap();
-            x.set_src("/modal.mp3");
-        }
-
-        assert_eq!(res.status(), 200);
-    });
+    // also fetching from server
+    c::lyrics::init();
+    c::shopping_list::init();
 
     rsx! {
-        // Router::<routes::Route> {}
-        div {
-            button {
-                "+1"
-            }
-            p {
-                "asd"
-            }
-        }
+        Router::<routes::Route> {}
     }
 }
