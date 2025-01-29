8300a59b76e214446a82644b5d1330b812fc3d43
Tamás Tóth <tomjtoth@gmail.com>
2025-01-29 21:51:55 +0200

    showing score in Arx

--- src/components/arx_fatalis/controls.rs
+++ src/components/arx_fatalis/controls.rs
@@ -5,6 +5,26 @@ use crate::components::arx_fatalis::models::SigCastSpells;
 #[component]
 pub fn Controls() -> Element {
     let spells = use_context::<SigCastSpells>();
+    let score = spells.read().score();
 
-    rsx! {}
+    rsx! {
+        "💎 {score}"
+        a {
+            class: "spells",
+            href: "https://wiki.arx-libertatis.org/Spells",
+            target: "_blank",
+            "📖"
+                //     {noti && (
+        //       <>
+        //         &nbsp;<sub>{noti.page}</sub>
+        //       </>
+        //     )}
+        }
+    }
+    //   {noti && (
+    //     <>
+    //       {noti.spell} ({noti.sequence.join(" + ")})
+    //     </>
+    //   )}
+    //   {/* TODO: <span id="reset-runes-score">reset</span> */}
 }
