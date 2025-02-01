abf0420002220c10f066289ab5818a44f67b3ad3
Tamás Tóth <tomjtoth@gmail.com>
2025-02-01 21:18:17 +0200

    refactored to CxRecipes

--- src/components/shopping_list/models/mod.rs
+++ src/components/shopping_list/models/mod.rs
@@ -3,13 +3,13 @@ mod items;
 mod recipes;
 
 pub use active::CxActive;
-pub use items::*;
-pub use recipes::*;
+pub use items::CxItems;
+pub use recipes::CxRecipes;
 
 pub static RECIPES_ID: &'static str = "slr";
 
 pub fn init() {
     CxActive::init();
     CxItems::init();
-    Recipes::init();
+    CxRecipes::init();
 }
