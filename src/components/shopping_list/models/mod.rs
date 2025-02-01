edd995f447cdb7ae4d340e77ba3c917c29f94fe3
Tamás Tóth <tomjtoth@gmail.com>
2025-02-01 18:23:47 +0200

    ShoppingList/CxActive done

--- src/components/shopping_list/models/mod.rs
+++ src/components/shopping_list/models/mod.rs
@@ -2,14 +2,14 @@ mod active;
 mod items;
 mod recipes;
 
-pub use active::*;
+pub use active::CxActive;
 pub use items::*;
 pub use recipes::*;
 
 pub static RECIPES_ID: &'static str = "slr";
 
 pub fn init() {
-    Active::init();
+    CxActive::init();
     Items::init();
     Recipes::init();
 }
