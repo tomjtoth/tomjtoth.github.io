e782db0c8ff5ab215074bb464ce22bbe740b1e8c
Tamás Tóth <tomjtoth@gmail.com>
2025-02-03 00:28:15 +0200

    main.rs cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,15 @@ mod hooks;
 mod routes;
 mod utils;
 
-use components::audio::AUDIO;
-use hooks as h;
-
 fn main() {
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
-    {
-        let _trip_init = AUDIO.read();
-    }
-
-    // init static resources
-    h::BatMon::init();
+    // tripping initialization on 1st use
+    components::audio::AUDIO.with(|_| {});
+    hooks::BatMon::init();
 
     rsx! {
         Router::<routes::Route> {}
