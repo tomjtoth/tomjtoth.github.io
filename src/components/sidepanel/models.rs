905df0e74c1c8a541f93725dbfbb215d9672f964
Tamás Tóth <tomjtoth@gmail.com>
2025-02-04 23:33:27 +0200

    sidepanel closing animation fixed

--- src/components/sidepanel/models.rs
+++ src/components/sidepanel/models.rs
@@ -1,15 +1,21 @@
+use std::time::Duration;
+
 use dioxus::prelude::*;
+use gloo_timers::future::sleep;
+use wasm_bindgen_futures::spawn_local;
 
 use crate::utils::LSCompatType;
 
 static KEY: &'static str = "sidepanel";
 type GsSidepanel = GlobalSignal<bool>;
-
 pub(crate) static SIDEPANEL: GsSidepanel = GlobalSignal::new(|| bool::load_t(KEY));
 
 pub(crate) trait TrSidepanel {
+    const DELAY: Duration = Duration::from_millis(10);
+
     fn show(&self);
     fn hide(&self);
+    fn hide_async(&self);
     fn set_to(&self, state: bool);
 }
 
@@ -20,6 +26,14 @@ impl TrSidepanel for GsSidepanel {
 
     fn hide(&self) {
         self.set_to(false);
+    }
+
+    /// successfully animates closing of SP while changing view
+    fn hide_async(&self) {
+        spawn_local(async {
+            sleep(Self::DELAY).await;
+            SIDEPANEL.hide();
+        });
     }
 
     fn set_to(&self, state: bool) {
