2efadfff1c257bff22a9bf787fa0f32142f45b18
Tamás Tóth <tomjtoth@gmail.com>
2025-02-13 23:55:39 +0200

    init separated from useShoppingList

--- src/hooks/shopping-list/init.ts
+++ src/hooks/shopping-list/init.ts
@@ -1,28 +1,19 @@
-import { useContext, useEffect } from "react";
+import { useEffect } from "react";
 
 import { useAppDispatch, useAppSelector } from "..";
-import { CxModal } from "../modal";
 import useSpinner from "../spinner";
 import { init } from "../../reducers/shopping-list";
 
 export default function useInit() {
   const spinner = useSpinner();
-  const modal = useContext(CxModal)!;
   const dispatch = useAppDispatch();
-
-  const rs = useAppSelector((s) => s.shoppingList);
-  const loaded = rs.recipes.length > 0;
+  const loaded = useAppSelector((s) => s.shoppingList.recipes.length > 0);
 
   useEffect(() => {
     if (!loaded) {
-      console.debug("initializing recipes, showing spinner");
-      dispatch(init());
       spinner.show();
-    } else {
-      console.debug("recipes loaded, hiding spinner");
-      spinner.hide();
+      console.debug("fetching recipes");
+      dispatch(init()).then(spinner.hide);
     }
-  }, [loaded]);
-
-  return { loaded, dispatch, modal, ...rs };
+  }, []);
 }
