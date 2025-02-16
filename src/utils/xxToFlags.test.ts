543103b377ec3bc9b103606c8de45925df0ffc24
Tamás Tóth <tomjtoth@gmail.com>
2025-02-16 16:12:33 +0200

    implemented functions xxToFlags and nameOf

---
+++ src/utils/xxToFlags.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from "vitest";
+
+import { xxToFlags as fn, nameOf } from ".";
+
+describe(nameOf(fn), () => {
+  it("replaces single occurence", () => {
+    expect(fn("::FI")).toBe("🇫🇮");
+  });
+
+  it("removes markups with invalid country code", () => {
+    expect(fn("::FI asdf ::QQ")).toBe("🇫🇮 asdf ");
+  });
+
+  it("replaces multiple occurences correctly", () => {
+    expect(fn("11 ::HU 22 ::FI")).toBe("11 🇭🇺 22 🇫🇮");
+  });
+
+  it("requires a space on the left", () => {
+    expect(fn("11 ::HU 22 ::FI 33::FR")).toBe("11 🇭🇺 22 🇫🇮 33::FR");
+  });
+});
