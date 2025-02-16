a8da738972199708ab1060a3b10a7e47056de3a9
Tamás Tóth <tomjtoth@gmail.com>
2025-02-16 17:14:31 +0200

    unified Sidepanel link generation and AppRoutes

---
+++ src/components/Routes/config.tsx
@@ -0,0 +1,67 @@
+import Home from "../Home";
+import CV from "../CV";
+import BatteryMonitor from "../BatteryMonitor";
+import Lyrics from "../Lyrics";
+import ShoppingList from "../ShoppingList";
+import ArxFatalis from "../ArxFatalis";
+import Visitors from "../Visitors";
+import Luxor from "../Luxor";
+import { Navigate } from "react-router";
+
+type RouteDetails = {
+  path: string;
+  element: React.ReactElement;
+  title?: string;
+  lang?: string;
+};
+
+export const ROUTES_CONFIG: RouteDetails[] = [
+  {
+    path: "/",
+    element: <Home />,
+    title: "alkuun",
+  },
+  {
+    path: "/cv",
+    element: <CV />,
+    title: "Curriculum Vitae",
+    lang: "en",
+  },
+  {
+    path: "/visitors",
+    element: <Visitors />,
+    title: "látogatók",
+    lang: "hu",
+  },
+  {
+    path: "/luxor",
+    element: <Luxor />,
+    title: "Luxor sorsolás",
+    lang: "hu",
+  },
+  {
+    path: "/battery-monitor",
+    element: <BatteryMonitor />,
+    title: "akunvalvonta",
+  },
+  {
+    path: "/shopping-list",
+    element: <ShoppingList />,
+    title: "ostoslista",
+  },
+  {
+    path: "/lyrics",
+    element: <Lyrics />,
+    title: "låttext",
+    lang: "sv",
+  },
+  {
+    path: "/arx-fatalis",
+    element: <ArxFatalis />,
+    title: "Arx Fatails minipeli",
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" />,
+  },
+];
