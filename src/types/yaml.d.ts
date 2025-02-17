a2afcbd2805df723c6efab38e3f67d7f6405b806
Tamás Tóth <tomjtoth@gmail.com>
2025-02-17 20:09:11 +0200

    rm @rollup/yaml-plugin and import every yaml file with `?raw` suffix

--- src/types/yaml.d.ts
+++
@@ -1,6 +0,0 @@
-// required for importing yaml files
-
-declare module "*.yaml" {
-  const value: any;
-  export default value;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 import terser from "@rollup/plugin-terser";
-import yaml from "@rollup/plugin-yaml";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig(() => ({
-  plugins: [yaml()],
+  plugins: [],
   test: {
     globals: true,
     coverage: {
