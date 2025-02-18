bf67ecdb2504e8781c78d8819929c9922f646f83
Tamás Tóth <tomjtoth@gmail.com>
2025-02-18 11:32:21 +0200

    use BrowserRouter instead of HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 import { Provider } from "react-redux";
 
 import { store } from "./store";
@@ -28,9 +28,9 @@ if ("serviceWorker" in navigator) {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
