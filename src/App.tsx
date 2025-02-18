9c28eaf6731dcd1d4429dfef1bc2efa212410992
Tamás Tóth <tomjtoth@gmail.com>
2025-02-18 22:12:43 +0200

    created common div directly under div#root for unified handling of events, like dropping files

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import useBatMonDaemon from "./hooks/battery-monitor/daemon";
 import Modal from "./components/Modal";
 import Spinner from "./components/Spinner";
 import Sidepanel from "./components/Sidepanel";
-import AppRoutes from "./components/Routes";
+import AppRoutes from "./components/AppRoutes";
+import AppRoot from "./components/AppRoot";
 
 export default function App() {
   useBatMonDaemon();
 
   return (
     <Modal>
-      <Spinner />
-      <Sidepanel />
-      <AppRoutes />
+      <AppRoot>
+        <Spinner />
+        <Sidepanel />
+        <AppRoutes />
+      </AppRoot>
     </Modal>
   );
 }
