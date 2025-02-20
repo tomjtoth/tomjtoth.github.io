294c4d56b23532638c6d2b4fc387c2a84daa26ff
Tamás Tóth <tomjtoth@gmail.com>
2025-02-20 19:12:37 +0200

    reminders every 7 days via modal on the 3 clickable buttons in CV

--- src/components/CV/Controls.tsx
+++ src/components/CV/Controls.tsx
@@ -1,17 +1,30 @@
+import { useRef } from "react";
+
 import { useAppSelector } from "../../hooks";
 import useModal from "../../hooks/modal";
 import { useFilesToCV } from "./logic";
+import { getCookie, setCookie } from "../../utils";
 
 export default function Controls() {
   const filesToCV = useFilesToCV();
   const url = useAppSelector((s) => s.cv.url);
   const modal = useModal();
 
+  const uploadRef = useRef<HTMLInputElement | null>(null);
+  const downloadRef = useRef<HTMLAnchorElement | null>(null);
+
   return (
     <>
+      <a
+        id="cv-download"
+        ref={downloadRef}
+        download="cv-template.yaml"
+        href={url}
+      />
       <input
+        id="cv-upload"
+        ref={uploadRef}
         type="file"
-        id="cv-file-upload"
         className="cv"
         accept="image/*,.yaml"
         onChange={(e) => {
@@ -24,33 +37,88 @@ export default function Controls() {
         }}
         multiple
       />
-      <label className="padded bordered clickable" htmlFor="cv-file-upload">
-        <b>drag/upload your CV here</b>
-      </label>
-      <a
-        download="cv-template.yaml"
-        href={url}
-        onClick={() =>
-          modal
-            .ok()
-            .en()
-            .prompt(
-              <p>
-                Edit the downloaded template{" "}
-                <u>
-                  <b>paying attention to indentation</b>
-                </u>
-                , then re-upload it along with a pic
-              </p>
-            )
-        }
+
+      <span
+        className="clickable padded bordered"
+        onClick={() => {
+          const cookie = "cv-downloaded";
+          const reminded = Number(getCookie(cookie) ?? "0");
+
+          if (reminded < 2) {
+            modal
+              .en()
+              .ok(downloadRef.current?.click)
+              .cancel()
+              .prompt(
+                <p>
+                  Edit the downloaded template{" "}
+                  <u>
+                    <b>paying attention to indentation</b>
+                  </u>
+                  , then re-upload it along with a pic.
+                </p>
+              );
+          } else {
+            // do not prompt more than 2x within 7 days
+            downloadRef.current?.click();
+          }
+          setCookie(cookie, reminded + 1);
+        }}
       >
-        template
-      </a>
+        download this template
+      </span>
+
+      <span
+        className="padded bordered clickable"
+        onClick={() => {
+          const cookie = "cv-uploaded";
+          const reminded = Number(getCookie(cookie) ?? "0");
+
+          if (reminded < 2) {
+            modal
+              .en()
+              .ok(uploadRef.current?.click)
+              .cancel()
+              .prompt(
+                <p>
+                  You can pick any image or <code>*.yaml</code> files, only the
+                  first one of both types will be recognized. Alternatively you
+                  can drag and drop files anywhere.
+                </p>
+              );
+          } else {
+            // do not prompt more than 2x within 7 days
+            uploadRef.current?.click();
+          }
+          setCookie(cookie, reminded + 1);
+        }}
+      >
+        <b>upload your CV</b>
+      </span>
       <span
         className="clickable padded"
         title="print (Ctrl + P)"
-        onClick={print}
+        onClick={() => {
+          const cookie = "cv-printed";
+          const reminded = Number(getCookie(cookie) ?? "0");
+
+          if (reminded < 2) {
+            modal
+              .en()
+              .ok(print)
+              .cancel()
+              .prompt(
+                <p>
+                  I recommend setting a 94% scale and default marings, that was
+                  working fine for me
+                </p>
+              );
+          } else {
+            // do not prompt more than 2x within 7 days
+            print();
+          }
+          setCookie(cookie, reminded + 1);
+        }}
       >
         🖨️
       </span>
