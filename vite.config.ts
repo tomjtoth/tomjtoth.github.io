9842c6dd5054854d28aa1a30268040d98e61379f
Tamás Tóth <tomjtoth@gmail.com>
2025-02-21 17:38:02 +0200

    added jsdom as devDeps

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(() => ({
   plugins: [],
   test: {
     globals: true,
+    environment: "jsdom",
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
