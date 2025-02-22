7274c9a693e6606c34b8e29df7915b5d7cf8b32f
Tamás Tóth <tomjtoth@gmail.com>
2025-02-22 16:49:06 +0200

    npm install tailwindcss @tailwindcss/vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 import terser from "@rollup/plugin-terser";
+import tailwindcss from "@tailwindcss/vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig(() => ({
-  plugins: [],
+  plugins: [tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
