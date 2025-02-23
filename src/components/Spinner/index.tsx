50a0f2a7f8f4ffb80e8f2f7a70e6b7cfe717b26f
Tamás Tóth <tomjtoth@gmail.com>
2025-02-23 16:48:56 +0200

    <Spinner /> returns null instead of false when not rendered

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -7,29 +7,27 @@ export function Spinner() {
   const { active, className } = useAppSelector((s) => s.spinner);
   const dispatch = useAppDispatch();
 
-  return (
-    active && (
-      <div className="modal-blur">
-        <div
-          {...{
-            id: "spinner",
-            className,
-            onAnimationEnd: (ev) => {
-              console.debug("animation", ev.animationName, "ended");
-              if (ev.animationName === "zoom-in") {
-                dispatch(resetSpinner());
+  return active ? (
+    <div className="modal-blur">
+      <div
+        {...{
+          id: "spinner",
+          className,
+          onAnimationEnd: (ev) => {
+            console.debug("animation", ev.animationName, "ended");
+            if (ev.animationName === "zoom-in") {
+              dispatch(resetSpinner());
 
-                // React's async state change is too slow
-                const circle = ev.target as HTMLDivElement;
-                const blur = circle.parentNode as HTMLDivElement;
-                [circle, blur].forEach(
-                  (div) => (div.style.visibility = "hidden")
-                );
-              }
-            },
-          }}
-        />
-      </div>
-    )
-  );
+              // React's async state change is too slow
+              const circle = ev.target as HTMLDivElement;
+              const blur = circle.parentNode as HTMLDivElement;
+              [circle, blur].forEach(
+                (div) => (div.style.visibility = "hidden")
+              );
+            }
+          },
+        }}
+      />
+    </div>
+  ) : null;
 }
