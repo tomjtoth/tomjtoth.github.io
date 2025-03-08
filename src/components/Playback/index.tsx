48820bee3068791ca46c6f25ebbb307a9e7bdcd3
Tamás Tóth <tomjtoth@gmail.com>
2025-03-08 16:40:30 +0200

    inserting `div.grow` in front of Playback control conditionally based on context

--- src/components/Playback/index.tsx
+++ src/components/Playback/index.tsx
@@ -1,8 +1,23 @@
+import { createContext, useContext } from "react";
+
 import { QuotesControls } from "./Quotes";
 import { SpeechControls } from "./Speech";
+
+export const CxMediaControlsPadding = createContext<boolean>(true);
 
 export function MediaControls() {
   const speech = SpeechControls();
   const quotes = QuotesControls();
-  return speech ?? quotes;
+  const rendered = speech !== null || quotes !== null;
+
+  const needsPadding = useContext(CxMediaControlsPadding);
+
+  const padding = rendered && needsPadding ? <div className="grow" /> : null;
+
+  return (
+    <>
+      {padding}
+      {speech ?? quotes}
+    </>
+  );
 }
