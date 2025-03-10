5e1b69987c1823e71d304f8de32314aea3d7d133
Tamás Tóth <tomjtoth@gmail.com>
2025-03-10 10:13:45 +0200

    simplified re-exports of types/arx/{runes,spells}.ts

--- src/types/arx-fatalis/index.ts
+++ src/types/arx-fatalis/index.ts
@@ -1,5 +1,5 @@
-export { Rune, RuneEnum } from "./runes";
-export { Spell, SpellEnum } from "./spells";
+export * from "./runes";
+export * from "./spells";
 
 export type State = {
   loaded: boolean;
