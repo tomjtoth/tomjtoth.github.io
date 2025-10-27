ca6ab9013731edbe4760ac54c5e8d38a9744ba3d
Tamás Tóth <tomjtoth@gmail.com>
2025-10-27 13:40:07 +0200

    migrate Albums.tsx to use hashes

--- src/components/Lyrics/Albums.tsx
+++ src/components/Lyrics/Albums.tsx
@@ -5,20 +5,26 @@ import { tLyr } from "../../reducers";
 import Songs from "./Songs";
 import Logo from "./Logos";
 
-export default function Albums({ artistIdx, albums }: AlbumsProps) {
+export default function Albums({
+  artistIdx,
+  albums,
+  parentHashes,
+}: AlbumsProps) {
   const active = useAppSelector((s) => s.lyrics.active);
   const dispatch = useAppDispatch();
 
   return (
     <ul>
-      {albums.map(({ title, year, url, songs }, albumIdx) => {
-        const id = [artistIdx, albumIdx].join("-");
+      {albums.map(({ title, year, url, songs, hash }, albumIdx) => {
+        const hashes = parentHashes.concat(hash);
+
+        const id = hashes.join("-");
 
         const clickable = albums.length > 1;
 
         return (
           <li
-            key={albumIdx}
+            key={hash}
             {...{
               className: `${
                 clickable ? "clickable " : "non-clickable "
@@ -34,7 +40,7 @@ export default function Albums({ artistIdx, albums }: AlbumsProps) {
             {year && `${year} - `}
             {title === "null" ? "mix" : title}
             <Logo {...{ url }} />
-            <Songs {...{ artistIdx, albumIdx, songs }} />
+            <Songs {...{ artistIdx, albumIdx, songs, parentHashes: hashes }} />
           </li>
         );
       })}
