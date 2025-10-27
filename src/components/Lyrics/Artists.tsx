6adfda4ec0599838eed45416499a85a0479bc312
Tamás Tóth <tomjtoth@gmail.com>
2025-10-27 13:39:45 +0200

    use hashes in Artists.tsx

--- src/components/Lyrics/Artists.tsx
+++ src/components/Lyrics/Artists.tsx
@@ -11,24 +11,22 @@ export default function Artists() {
 
   return (
     <ul lang="sv" id="lyrics" className="pl-0 [&_a]:p-2 [&_a]:ml-2">
-      {artists.map(({ name, url, albums }, artistIdx) => {
-        const id = [artistIdx].join();
-
+      {artists.map(({ name, url, albums, hash }, artistIdx) => {
         return (
           <li
-            key={artistIdx}
+            key={hash}
             {...{
               className: `clickable p-4 border rounded${
-                active.includes(id) ? " active" : ""
+                active.includes(hash) ? " active" : ""
               }`,
               onClick: (e) => {
-                if (e.target === e.currentTarget) dispatch(tLyr.toggle(id));
+                if (e.target === e.currentTarget) dispatch(tLyr.toggle(hash));
               },
             }}
           >
             {name}
             <Logo {...{ url }} />
-            <Albums {...{ artistIdx, albums }} />
+            <Albums {...{ artistIdx, albums, parentHashes: [hash] }} />
           </li>
         );
       })}
